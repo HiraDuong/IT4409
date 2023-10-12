dac9ad745603e25bbfc6397b7d2167196ed233e7
Vũ Huy Hoàng <115575325+HiraDuong@users.noreply.github.com>
2023-10-12 15:36:27 +0700

    Push

--- Week4/Onclass/cttprofile/main.js
+++ Week4/Onclass/cttprofile/main.js
@@ -1,6 +1,9 @@
 var tt_text = document.getElementById('tt-text');
 var ql_text = document.getElementById('ql-text');
 
+const nav2IdArray = ['ll-text', 'qd-text', 'kt-text', 'lh-text', 'nx-text', 'hdtg-text'];
+
+
 function display_content(elementId) {
   var element = document.getElementById(elementId);
   if(element == tt_text){
@@ -16,4 +19,31 @@ function display_content(elementId) {
     } 
   }
  
+}
+
+function display_content_two(elementId) {
+  var element = document.getElementById(elementId);
+  if (element.style.display === 'none') {
+
+    for (let i = 0; i < nav2IdArray.length; i++) {
+      if(nav2IdArray[i] !== elementId){
+        var curElement = document.getElementById(nav2IdArray[i])
+        curElement.style.display = 'none';
+      }
+    }
+    element.style.display = 'block';
+
+  }
+
+
+}
+var isSearchBoxOpen = false;
+
+
+function toggleSearch() {
+  var searchBox = document.querySelector('.search-box');
+  if (!isSearchBoxOpen) {
+    searchBox.style.width = '200px';
+    isSearchBoxOpen = true;
+  }
 }
